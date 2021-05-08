c727aa552d4fe08ba0ac7fc2b3f084052712a63a
Mason Xiong <mason.xiong@yahoo.com>
2021-05-08 21:46:18 +1000

    update note

--- 3-express/learn-express/index.js
+++ 3-express/learn-express/index.js
@@ -1,6 +1,49 @@
 const express = require('express');
 const app = express();
 
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  // req.query;
+  // req.body;
+  // req.params
+  // res.send('reject');
+  req.time = new Date();
+
+  console.log(1);
+  next();
+});
+
+function m2(req, res, next) {
+  console.log(2);
+  next();
+}
+
+app.use(m2);
+
+// m2
+// app.use('/v1',(req, res, next) => {
+
+// })
+// app.get('/v1', (req, res) => { });
+
+// app.use((req, res, next) => {});
+
+// PUT VS PATCH
+/**
+ * {
+ *    "name":"mason",
+ *    "gender": "male"
+ * }
+ *
+ * PUT , body: { "name": "alice" }
+ *       ->    { "name": "alice" }
+ *
+ * PATCH, body: { "name": "alice" }
+ *      ->     { "name": "alice", "gender": "male" }
+ */
+
 // url, href
 // /api/v1/tasks?username=mason
 // path, query params
@@ -21,15 +64,61 @@ const app = express();
 // /api/greeting/james
 // 变量名需要一一对应
 // endpoint
+app.use('/api/greeting/:name', m3);
 app.get('/api/greeting/:name', (req, res) => {
   const { name } = req.params;
   const { title } = req.query;
   res.send(`${title}.${name}`);
 });
-// app.post();
+
+function m3(req, res, next) {
+  console.log(3);
+  next();
+}
+
+// [m1,m2,m3,RH]
+app.use('/api/students', m3);
+app.post('/api/students', (req, res) => {
+  // throw new Error('an error');
+
+  // validate req.body
+  // 1. res.sendStatus(400)
+  // 2. throw error
+  // req.body;
+  res.send({ ...req.body, time: req.time }); // payload
+});
 // app.put();
 // app.delete();
+
+// [m1, ...., RH, m10]
+app.use((req, res, next) => {
+  // req.query;
+  // req.body;
+  // req.params
+  res.send('reject');
+  // next();
+});
+
+// 当server出错的时候，对相应的错误进行抓取，然后返回正确的状态信息
+function errorHandler(error, req, res, next) {
+  console.log(error);
+  res.sendStatus(500);
+}
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('server listening on port 3000');
 });
+
+// try {
+//   foo();
+// } catch (e) {
+
+// }
+
+// foo() {
+//   if (//some condition) {
+//     throw new Error()
+//   }
+// }
---
+++ 4-api-server/prac/index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+/**
+ * {
+ *    id: number,
+ *    description: string,
+ *    done: boolean
+ * }
+ */
+const tasks = [];
+let id = 1;
+
+app.use(cors);
+
+// push, pop, unshift, shift
+// splice 进行中间删除或添加的时候
+// slice 是取subarray
+// find, findIndex, filter, map, some, reduce
+
+app.get('/tasks', (req, res) => {
+  const { description } = req.query;
+  if (description) {
+    const filteredTasks = tasks.filter((i) =>
+      i.description.includes(description)
+    );
+    return res.json(filteredTasks);
+  }
+  return res.json(tasks);
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const { id } = req.params; // string
+  const task = tasks.find((i) => i.id === Number(id));
+  if (!task) {
+    return res.sendStatus(404);
+  }
+  return res.json(task);
+});
+
+app.post('/tasks', (req, res) => {
+  const { description } = req.body;
+  // id++ => id+=1; =>  id = id+1;
+  // ++id, id现在为 2
+  const task = { description, id: id++, done: false };
+  // id++, id现在为 2
+  tasks.push(task);
+  return res.json(task);
+});
+
+app.put('/tasks/:id', (req, res) => {
+  // const { id, description, done } = req.params;
+  const { id } = req.params;
+  const { description, done } = req.body;
+
+  let task = tasks.find((i) => i.id === Number(id));
+  if (!task) {
+    return res.sendStatus(404);
+  }
+
+  // task = { id, description, done };
+  // tasks.push(task);
+  // if (description) {
+  //   task.description = description;
+  // }
+  // if (done !== undefined) {
+  //   task.done = !!done;
+  // }
+  const newTask = {
+    id: Number(id),
+    description: description || task.description,
+    done: !!done || task.done
+  };
+  const taskIndex = tasks.findIndex((i) => i.id === Number(id));
+  tasks[taskIndex] = newTask;
+  return res.json(newTask);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  const taskIndex = tasks.findIndex((i) => i.id === Number(id));
+  if (taskIndex === -1) {
+    return res.sendStatus(404);
+  }
+  tasks.splice(taskIndex, 1);
+  return res.sendStatus(204);
+});
+
+app.listen(3000, () => {
+  console.log('server listening on port 3000');
+});
+
+function cors(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  next();
+}
+
+/**
+ * {id: number, author: string, content: string}
+ */
