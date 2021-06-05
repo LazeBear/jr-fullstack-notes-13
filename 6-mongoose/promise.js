b7af079746d6402e442bad704fb7ec6447e54ef9
Mason Xiong <mason.xiong@yahoo.com>
2021-06-05 21:38:01 +1000

    update notes

--- 6-mongoose/promise.js
+++ 6-mongoose/promise.js
@@ -1,7 +1,7 @@
 function asyncSum(a, b, cb) {
   setTimeout(() => {
-    console.log(a + b);
-    cb();
+    // console.log(a + b);
+    cb(a + b);
   }, Math.random() * 1000);
 }
 
@@ -17,31 +17,48 @@ function promiseSum(a, b) {
   // resolve, reject
   return new Promise((res, rej) => {
     setTimeout(() => {
-      // res(a + b);
-      rej('error here');
+      res(a + b);
+      // rej('error here');
     }, Math.random() * 1000);
   });
 }
 
-promiseSum(1, 1)
-  .then(
-    (result) => {
-      console.log('result', result);
-      console.log('error', reject);
-      return 1;
-    },
-    (error) => {
-      return 'error';
-    }
-  )
-  .catch((error) => {
-    return 'error';
-  })
-  .then((number) => {
-    // number === 1;
-    console.log(number);
-    return 2;
-  })
-  .catch((error) => {
-    console.log('error', error);
-  });
+// promiseSum(1, 1)
+//   .then(
+//     (result) => {
+//       console.log('result', result);
+//       console.log('error', reject);
+//       return 1;
+//     },
+//     (error) => {
+//       return 'error';
+//     }
+//   )
+//   .catch((error) => {
+//     return 'error';
+//   })
+//   .then((number) => {
+//     // number === 1;
+//     console.log(number);
+//     return 2;
+//   })
+//   .catch((error) => {
+//     console.log('error', error);
+//   });
+
+// syntax sugar
+async function asyncCall() {
+  try {
+    // let result = await promiseSum(1, 2);
+    // result = await promiseSum(result, 2);
+    // result = await promiseSum(result, 3);
+    // console.log(result);
+    // const a = await foo();
+    // const b = await bar();
+    // a + b;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+asyncCall();
---
+++ jr-cms/readme.md
@@ -0,0 +1,8 @@
+cms-> content management system
+
+users -> client
+students
+M -> N
+courses
+M -> N
+teachers
